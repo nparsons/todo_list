051db72a1f6bf3064ca66425a9ad6e5151a323bb
nparsons <parsons.nicholas1@gmail.com>
2020-12-09 13:46:09 -0500

    Added entries function to module and new unit test.

--- test/todo_list_test.exs
+++ test/todo_list_test.exs
@@ -6,7 +6,7 @@ defmodule TodoListTest do
     assert TodoList.new() == %{}
   end
 
-  test "adds entry for non-existant title" do
+  test "adds entry for non-existant date" do
     expected_title = "Dentist"
     expected_date = ~D[2020-12-09]
     test_list = TodoList.new()
@@ -20,7 +20,37 @@ defmodule TodoListTest do
       )
 
     assert Enum.count(test_list) > 0
-    actual_title = hd(Map.get(test_list, expected_date))
+    actual_title = hd(TodoList.entries(test_list, expected_date))
+    assert expected_title == actual_title
+  end
+
+  test "adds entry for existing date" do
+    expected_title = "Dentist"
+    expected_date = ~D[2020-12-09]
+
+    test_list =
+      TodoList.new()
+      |> TodoList.add_entry(
+        expected_date,
+        "Eat breakfast"
+      )
+
+    assert Enum.count(test_list) > 0
+
+    test_list =
+      TodoList.add_entry(
+        test_list,
+        expected_date,
+        expected_title
+      )
+
+    assert Enum.count(TodoList.entries(test_list, expected_date)) == 2
+
+    actual_title =
+      TodoList.entries(test_list, expected_date)
+      |> Enum.filter(&(&1 == "Dentist"))
+      |> hd()
+
     assert expected_title == actual_title
   end
 end
