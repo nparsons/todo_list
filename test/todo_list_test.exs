fcfa2751ac24c63970753efca0fe184629676e7e
nparsons <parsons.nicholas1@gmail.com>
2020-12-10 11:44:24 -0500

    Added update function to module and unit tests.

--- test/todo_list_test.exs
+++ test/todo_list_test.exs
@@ -37,4 +37,35 @@ defmodule TodoListTest do
 
     assert expected_title == actual_entry.title
   end
+
+  test "updates entry with non-existance id" do
+    test_list = TodoList.new()
+    assert test_list == TodoList.update_entry(test_list, -44, &(&1 + 1))
+  end
+
+  test "updates entry title with existing id" do
+    expected_title = "Dentist"
+    original_title = "Get Dressed"
+    test_date = ~D[2020-12-10]
+
+    test_list =
+      TodoList.new()
+      |> TodoList.add_entry(%{date: test_date, title: original_title})
+
+    test_list = TodoList.update_entry(test_list, 1, &Map.put(&1, :title, expected_title))
+
+    updated_entry =
+      TodoList.entries(test_list, test_date)
+      |> Enum.filter(&(&1.title == expected_title))
+      |> hd()
+
+    assert expected_title == updated_entry.title
+
+    original_title_count =
+      TodoList.entries(test_list, test_date)
+      |> Enum.filter(&(&1.title == original_title))
+      |> Enum.count()
+
+    assert 0 == original_title_count
+  end
 end
